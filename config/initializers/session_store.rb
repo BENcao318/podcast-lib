41f8c96bdb3f50cda3abc87107bd90050c47a0c7
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-07-02 23:44:09 -0400

    Change session_store domain to all domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_authentication_podcast_user', domain: "https://my-podcast-lib-app.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: '_authentication_podcast_user', domain: :all
 else
-  Rails.application.config.session_store :cookie_store, key: '_authentication_podcast_user'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_podcast_user', domain: :all
   
 end
