5a00b88c4c7282149b33dd19de666ebb9378221a
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-07-03 09:04:02 -0400

    Enable the disable_request_forgery_protection in config development

--- client/src/components/GenreBox.jsx
+++ client/src/components/GenreBox.jsx
@@ -7,7 +7,7 @@ function GenreBox({ podcastGenres }) {
   const navigate = useNavigate()
 
   return (
-    <div className='flex gap-2 mx-2'>
+    <div className='flex gap-2 mx-2 items-center'>
       {
         genres.map((genre) => (
           <div key={genre.id} className={`${genreBackgroundColor(genre.name)} text-semibold text-white rounded-xl p-1 w-24 cursor-pointer`} onClick={() => navigate(`/genres/${genre.name}/${genre.id}`)}>
