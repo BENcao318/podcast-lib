40c7fdcf9e716b9667f29ae3321205e70beb4fbb
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-07-04 14:59:28 -0400

    Change the url settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
@@ -26,15 +26,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   
   allow do
     origins 'https://wondrous-mandazi-28ebe8.netlify.app'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
-  
-  allow do
-    origins 'https://podcast-lib.herokuapp.com'
 
     resource '*',
       headers: :any,
