86acb24f887db0d10f00f846a51d6ddbedb59143
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-07-04 15:15:24 -0400

    Add skip_before_filter

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_podcast_user', domain: :all, same_site: :none, secure: true, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_authentication_podcast_user', domain: :all, tld_length: 2
